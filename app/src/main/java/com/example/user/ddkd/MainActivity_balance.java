5cc4443d087782606bfa32f206f6d1e2a544b142
8892966 <980239733@qq.com>
2016-04-06 19:30:49 +0800

    第一版

---
+++ app/src/main/java/com/example/user/ddkd/MainActivity_balance.java
@@ -0,0 +1,93 @@
+package com.example.user.ddkd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class MainActivity_balance extends Activity implements View.OnClickListener {
+
+    private List<Person> list;
+    private TextView textView;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_balance);
+
+        textView=(TextView)findViewById(R.id.TX);
+        textView.setOnClickListener(this);
+
+        ListView viewById = (ListView) findViewById(R.id.listviewbalance);
+        Person person=new Person();
+        person.setMoney("+1");
+        person.setReason("小费");
+        person.setAddr("南区三栋");
+        Person person2=new Person();
+        person2.setMoney("+2");
+        person2.setReason("小费");
+        person2.setAddr("南区七栋");
+        list=new ArrayList<Person>();
+        list.add(person);
+        list.add(person2);
+        viewById.setAdapter(new MyAdapter());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.TX:
+                Intent intent=new Intent(this,MainActivity_getmoney.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+//            Log.i("MainActivity_balance", list.size() + "");
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if(convertView==null){
+                LayoutInflater inflater = MainActivity_balance.this.getLayoutInflater();
+                view = inflater.inflate(R.layout.balance_listview, null);
+            }else{
+                view=convertView;
+            }
+            TextView view2=(TextView)view.findViewById(R.id.money);
+            TextView view1= (TextView) view.findViewById(R.id.reason);
+            TextView view3=(TextView)view.findViewById(R.id.addr);
+
+            Person person=list.get(position);
+            view1.setText(person.getMoney());
+            view2.setText(person.getReason());
+            view3.setText(person.getAddr());
+            return view;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/user/ddkd/MainActivity_getmoney.java
@@ -0,0 +1,14 @@
+package com.example.user.ddkd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class MainActivity_getmoney extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balance_getmoney);
+    }
+}

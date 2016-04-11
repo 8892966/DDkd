f621eea00a68c721804d76f5c2792a019b763815
8892966 <980239733@qq.com>
2016-04-11 22:56:34 +0800

    添加了px与dp的转换的工具类，注册3页面的图片可以压缩保存

--- app/src/main/java/com/example/user/ddkd/ZhuCe3Activity.java
+++ app/src/main/java/com/example/user/ddkd/ZhuCe3Activity.java
@@ -3,11 +3,14 @@ package com.example.user.ddkd;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.text.method.CharacterPickerDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +21,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.user.ddkd.beam.ZhuCeInfo;
+import com.example.user.ddkd.utils.DensityUtil;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * Created by User on 2016-04-03.
@@ -34,16 +41,19 @@ public class ZhuCe3Activity extends Activity implements View.OnClickListener {
     private ImageView iv_touxiang;
     private Spinner sp_diqu;
     private Spinner sp_loudong;
+    private String fileName = "";//帮助文件
+    private File tempFile;
 
     //地区
     private String[] mItems = {"中区", "南区", "东区"};
     //楼栋
-    private String[][] mItems2 = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+    private String[][] mItems2 = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zhuce3_activity);
+        initFile();
 
         iv_touxiang = (ImageView) findViewById(R.id.iv_touxiang);
         iv_touxiang.setOnClickListener(this);
@@ -59,27 +69,32 @@ public class ZhuCe3Activity extends Activity implements View.OnClickListener {
         et_xuehao = (EditText) findViewById(R.id.et_xuehao);//学号
 
         //**************************判断是否是在注册页面4返回回来的，如果是回显数据
-        ZhuCeInfo zhuCeInfo= (ZhuCeInfo) getIntent().getSerializableExtra("zhuCeInfo");
-        zhuCeInfo.getUsername();
-        zhuCeInfo.getNumber();
-        zhuCeInfo.getCollege();
+        ZhuCeInfo zhuCeInfo = (ZhuCeInfo) getIntent().getSerializableExtra("zhuCeInfo");
+        if (zhuCeInfo != null) {
+            et_name.setText(zhuCeInfo.getUsername());
+            et_phone.setText(zhuCeInfo.getNumber());
+            et_xueyuan.setText(zhuCeInfo.getCollege());
+        }
         //*******************
         //**********************
         //下拉列表实现
         // 建立Adapter并且绑定数据源
         ArrayAdapter<String> _Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mItems);
         //绑定 Adapter到控件
-        sp_diqu.setAdapter(_Adapter);
         sp_diqu.setPrompt("地区");
+        sp_diqu.setAdapter(_Adapter);
+
         sp_diqu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // 建立Adapter并且绑定数据源
                 ArrayAdapter<String> _Adapter2 = new ArrayAdapter<String>(ZhuCe3Activity.this, android.R.layout.simple_spinner_item, mItems2[position]);
                 //绑定 Adapter到控件
-                sp_loudong.setAdapter(_Adapter2);
                 sp_loudong.setPrompt("楼栋");
+                sp_loudong.setAdapter(_Adapter2);
+
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -94,12 +109,12 @@ public class ZhuCe3Activity extends Activity implements View.OnClickListener {
     public void next(View v) {
 //        Toast.makeText(this, sp_diqu.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
         //注册信息
-        ZhuCeInfo zhuCeInfo= (ZhuCeInfo) getIntent().getSerializableExtra("zhuCeInfo");
+        ZhuCeInfo zhuCeInfo = (ZhuCeInfo) getIntent().getSerializableExtra("zhuCeInfo");
         zhuCeInfo.setCollege(et_xueyuan.getText().toString());
         zhuCeInfo.setUsername(et_name.getText().toString());
         zhuCeInfo.setNumber(et_xuehao.getText().toString());
         Intent intent = new Intent(ZhuCe3Activity.this, ZhuCe4Activity.class);
-        intent.putExtra("zhuCeInfo",zhuCeInfo);//传递注册信息
+        intent.putExtra("zhuCeInfo", zhuCeInfo);//传递注册信息
         startActivity(intent);
         finish();
     }
@@ -114,39 +129,69 @@ public class ZhuCe3Activity extends Activity implements View.OnClickListener {
                 finish();
                 break;
             case R.id.iv_touxiang:
-                Uri uri=Uri.parse(Environment.getDataDirectory().getPath()+"/123.png");
-                crop(uri);
+//                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()+"/test.png");
+//                Log.e("uri",uri.toString());
+                getPhoto();
                 break;
         }
     }
 
-    private void crop(Uri uri){
-        Intent intent;
-        intent = new Intent("com.android.camera.action.CROP");
-//                intent.addCategory(Intent.CATEGORY_OPENABLE);
+    private void getPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);// 打开相册
+        intent.setDataAndType(MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
+        intent.putExtra("output",Uri.fromFile(tempFile));
+        startActivityForResult(intent,11);
+    }
+
+    private void crop(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, "image/*");
-        intent.putExtra("crop", "true");
+        intent.putExtra("output", Uri.fromFile(tempFile));
+        intent.putExtra("crop", true);
         intent.putExtra("aspectX", 1);
         intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", 250);
-        intent.putExtra("outputY", 250);
-//        intent.putExtra("data"，);
-
-        intent.putExtra("return-data", true);
-        startActivityForResult(intent, 0);
+        intent.putExtra("outputX", DensityUtil.px2dip(this,100));
+        intent.putExtra("outputY", DensityUtil.px2dip(this,100));
+        startActivityForResult(intent, 10);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println(resultCode);
-        if (data != null){
-            Bitmap cameraBitmap = (Bitmap) data.getExtras().get("data");
-            if (cameraBitmap != null) {
-                iv_touxiang.setImageBitmap(cameraBitmap);
-            }else {
-                Toast.makeText(ZhuCe3Activity.this, "获取图片出错，请再次获取", Toast.LENGTH_SHORT).show();
+//        System.out.println(resultCode);
+        if(requestCode==10) {
+            if (data != null) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap cameraBitmap = BitmapFactory.decodeFile(fileName, options);
+//                Bitmap cameraBitmap = (Bitmap) data.getExtras().get("data");
+                if (cameraBitmap != null) {
+                    iv_touxiang.setImageBitmap(cameraBitmap);
+                } else {
+                    Toast.makeText(ZhuCe3Activity.this, "获取图片出错，请再次获取", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else if(requestCode==11){
+            if(data!=null) {
+                Uri uri = data.getData();
+                Log.e("uri", uri.toString());
+                crop(uri);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void initFile() {
+        if(fileName.equals("")) {
+            boolean sdCardExist = Environment.getExternalStorageState()
+                    .equals(android.os.Environment.MEDIA_MOUNTED);
+            if(sdCardExist) {
+                String path = Environment.getExternalStorageDirectory().getPath();
+//                FileUtil.mkdir(path);
+//                Logger.i("path:" + path);
+                fileName = path + "/user_head_photo.jpg";
+                tempFile = new File(fileName);
+            } else {
+                Toast.makeText(this,"请插入SD卡",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }

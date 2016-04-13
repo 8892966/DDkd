989aa726ad881e215480af6450e9734c29c6d6a5
8892966 <980239733@qq.com>
2016-04-13 21:01:29 +0800

    个人信息栏的JSon解析步骤已添加

--- app/src/main/java/com/example/user/ddkd/details.java
+++ app/src/main/java/com/example/user/ddkd/details.java
@@ -11,51 +11,66 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.user.ddkd.text.DetailsInfo;
+import com.example.user.ddkd.text.UserInfo;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2016/4/4.
  */
 public class details extends Activity implements View.OnClickListener {
-    private List<DetailsInfo> list;
     private TextView exit_button;
+    private List<DetailsInfo> detailsinfolist;
+    private MyAdater myAdater;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_details);
         ListView listView=(ListView)findViewById(R.id.listviewdetails);
-        DetailsInfo detailsinfo=new DetailsInfo();
         exit_button=(TextView)findViewById(R.id.tv_head_fanghui);
         exit_button.setOnClickListener(this);
+        Volley_Get();
 
-        detailsinfo.setDetailsid(131110191);
-        detailsinfo.setDetailsMoney("2.5元");
-        detailsinfo.setUsername("刘嘉文");
-        detailsinfo.setCourier("省通快递");
-        detailsinfo.setTelphone(188139174);
-        detailsinfo.setAddr("南区七栋");
-        list=new ArrayList<DetailsInfo>();
-        list.add(detailsinfo);
-        listView.setAdapter(new MyAdater());
+        detailsinfolist=new ArrayList<DetailsInfo>();
+        myAdater = new MyAdater();
+        listView.setAdapter(myAdater);
+
+
+//        detailsinfo.setDetailsid(131110191);
+//        detailsinfo.setDetailsMoney("2.5元");
+//        detailsinfo.setUsername("刘嘉文");
+//        detailsinfo.setCourier("省通快递");
+//        detailsinfo.setTelphone(188139174);
+//        detailsinfo.setAddr("南区七栋");
+//        list=new ArrayList<DetailsInfo>();
+//        list.add(detailsinfo);
+//        listView.setAdapter(new MyAdater());
     }
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()){
             case R.id.tv_head_fanghui:
                 finish();
                 break;
         }
-
     }
 
     class MyAdater extends BaseAdapter{
         @Override
         public int getCount() {
-            return list.size();
+            return detailsinfolist.size();
         }
         @Override
         public Object getItem(int position) {
@@ -81,15 +96,44 @@ public class details extends Activity implements View.OnClickListener {
             TextView telephone=(TextView)view.findViewById(R.id.userphone);
             TextView addr=(TextView)view.findViewById(R.id.addr);
 
-            DetailsInfo detailsInfo=list.get(position);
-            orderid.setText(detailsInfo.getDetailsid()+"");
-            orderprice.setText(detailsInfo.getDetailsMoney());
+            DetailsInfo detailsInfo=detailsinfolist.get(position);
+            orderid.setText(detailsInfo.getId()+"");
+            orderprice.setText(detailsInfo.getPrice()+"");
             username.setText(detailsInfo.getUsername());
-            courier.setText(detailsInfo.getCourier());
-            telephone.setText(detailsInfo.getTelphone()+"");
-            addr.setText(detailsInfo.getAddr());
+            courier.setText(detailsInfo.getExpressCompany());
+            telephone.setText(detailsInfo.getPhone()+"");
+            addr.setText(detailsInfo.getAddressee());
+
+
+//            DetailsInfo detailsInfo=list.get(position);
+//            orderid.setText(detailsInfo.getDetailsid()+"");
+//            orderprice.setText(detailsInfo.getDetailsMoney());
+//            username.setText(detailsInfo.getUsername());
+//            courier.setText(detailsInfo.getCourier());
+//            telephone.setText(detailsInfo.getTelphone()+"");
+//            addr.setText(detailsInfo.getAddr());
 
             return view;
         }
+    }
+    public void Volley_Get(){
+        String url="http://www.louxiago.com/wc/ddkd/admin.php/Order/getOrder/state/0/uid/704?State=1";
+        StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Log.e("Volley_Get",s);
+                Type listv=new TypeToken<LinkedList<DetailsInfo>>(){}.getType();
+                Gson gson=new Gson();
+                detailsinfolist=gson.fromJson(s,listv);
+                myAdater.notifyDataSetChanged();//
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("111","内容为空");
+            }
+        });
+        request.setTag("abcGet");
+        MyApplication.getQueue().add(request);
     }
 }

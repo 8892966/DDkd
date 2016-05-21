6b412b9659c290ed0b33bd4810c560c862b64110
8892966 <980239733@qq.com>
2016-05-21 19:36:50 +0800

    5.21-19：36

--- app/src/main/java/com/example/user/ddkd/utils/UserInfoUtils.java
+++ app/src/main/java/com/example/user/ddkd/utils/UserInfoUtils.java
@@ -19,6 +19,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.example.user.ddkd.Announce;
 import com.example.user.ddkd.DingDanActivity;
+import com.example.user.ddkd.DingDanNewActivity;
 import com.example.user.ddkd.ExitApplication;
 import com.example.user.ddkd.MainActivity_balance;
 import com.example.user.ddkd.MainActivity_setting;
@@ -127,7 +128,7 @@ public class UserInfoUtils implements View.OnClickListener {
             case R.id.details:
                 //直接跳转到订单页面;
 //                intent = new Intent(MainActivity_main.this, details.class);
-                intent = new Intent(activity, DingDanActivity.class);
+                intent = new Intent(activity,DingDanNewActivity.class);
                 intent.putExtra("Static",3);
                 activity.startActivity(intent);
                 break;

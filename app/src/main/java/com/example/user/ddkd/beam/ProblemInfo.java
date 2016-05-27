b7840f0420ef7ed115513cd8e610abbd1bea09a2
8892966 <980239733@qq.com>
2016-05-27 19:31:12 +0800

    5.27 19:30

---
+++ app/src/main/java/com/example/user/ddkd/beam/ProblemInfo.java
@@ -0,0 +1,20 @@
+package com.example.user.ddkd.beam;
+
+/**
+ * Created by Administrator on 2016/5/26.
+ */
+public class ProblemInfo {
+    private String context;
+
+    public ProblemInfo(String context){
+        this.context=context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getContext() {
+        return context;
+    }
+}
